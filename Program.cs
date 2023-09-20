d93d8e1e31c75012a0664c246c74334a35aa44e0
Shawn <unshorn@gmail.com>
2023-09-21 08:28:04 +1200

    adds files

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿string StringJoin(string[] parts, string str2)
+{
+    string result = "";
+
+    for(int i = 0; i < parts.Length; i++)
+    {
+        string part = parts[i];
+        result = result + part;
+
+        if (i != (parts.Length - 1))
+            result = result + str2;
+    }
+
+    return result;
+} 
+string input = "The quick brown fox jumped over the lazy dog";
+string[] parts = input.Split();
+Console.WriteLine(StringJoin(parts, "-"));
+
